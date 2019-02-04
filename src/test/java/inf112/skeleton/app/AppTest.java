197aca899cd754726c39491950c1f623b3b5c52c
ingridjohansen <97johansen@gmail.com>
2019-02-04 14:55:26 +0100

    lagt til tester for map

--- src/test/java/inf112/skeleton/app/AppTest.java
+++ src/test/java/inf112/skeleton/app/AppTest.java
@@ -1,19 +1,43 @@
 package inf112.skeleton.app;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
+
+    //make map
+    int mapX = 10;
+    int mapY = 20;
+    Map map = new Map(mapX, mapY);
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
+    public void shouldAnswerWithTrue() {
         assertTrue( true );
+    }
+
+    //test for X-akse
+    @Test
+    public void testForX(){
+        //make map
+        assertEquals(map.getX(), 10);
+    }
+
+    //test for Y-akse
+    @Test
+    public void testForY(){
+        //make map
+        assertEquals(map.getY(), 20);
+    }
+
+    //test for robot
+    @Test
+    public void testForRobot(){
+        map.add('r', 5, 5);
+        assertTrue(map.isInstance('r', 5, 5));
     }
 }
