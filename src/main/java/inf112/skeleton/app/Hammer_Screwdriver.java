1011fab90b75edada97ddf61dca5505342838776
ingridjohansen <97johansen@gmail.com>
2019-02-06 19:00:23 +0100

    Added the rest og the classes for the board - Flag - Blue_Bong - Yellow_bond - Screwdriver - Hammer_Screwdriver - Laser - Void (Hole) - Rotating_Plate
    and fixed the map

---
+++ src/main/java/inf112/skeleton/app/Hammer_Screwdriver.java
@@ -0,0 +1,18 @@
+package inf112.skeleton.app;//Created by ingridjohansen on 06/02/2019.
+
+public class Hammer_Screwdriver implements IBoardObject{
+    @Override
+    public int getX() {
+        return 0;
+    }
+
+    @Override
+    public int getY() {
+        return 0;
+    }
+
+    @Override
+    public int color() {
+        return 0;
+    }
+}
