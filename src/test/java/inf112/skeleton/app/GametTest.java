de92ea3b495ce3708cf6c4be9a43c17a20b5982e
SandraSolberg <sandsol97@gmail.com>
2019-02-28 17:40:01 +0100

    Creating a before test setup

--- src/test/java/inf112/skeleton/app/GametTest.java
+++ src/test/java/inf112/skeleton/app/GametTest.java
@@ -1,14 +1,33 @@
 package inf112.skeleton.app;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import org.junit.Before;
 import org.junit.Test;
 import org.lwjgl.input.Keyboard;
+
+import java.awt.event.KeyEvent;
 
 import static org.junit.Assert.*;
 
 
 public class GametTest {
     private MapGUI map;
+    private Robot robot;
+
+
+
+    //Set up game and objects for test in this class
+    @Before
+
+    public static void setupMapBefore() throws Exception{
+        Map map = new Map(10, 20);
+    }
+
+
+
+
+
+
 
     @Test
 
@@ -38,7 +57,10 @@ public class GametTest {
 
     //TODO check if legal inputs are okay
 
-    public void keyBoardInputTest{
+    public void playerMovement(KeyEvent key){
+        Player testRobot = new Player(2, )
+
+
 
 
     }
@@ -51,11 +73,18 @@ public class GametTest {
     //Todo check if player goes through walls
 
     public void robotDoesNotGoThroughWalls(){
-       Player robot = new Player()
+       Player testRobot = new Player(0, 5, 5, Directions.DOWN);
+
+
+       assertFalse(testRobot.noticeWalls(Directions.DOWN));
+
+
+       Player noticeWalls = new Player(int numberOfFlags, Robot robot);
         robot.keyPressed(Directions.RIGHT);
 
-        map =  new LwjglApplication(new MapGUI(), cfg);
         Robot robot = new Robot(5, 5, Directions.UP);
+
+     assertFalse(noticeWalls.canGO(Directions.UP, instanceOf Wall));
 
 
     }
