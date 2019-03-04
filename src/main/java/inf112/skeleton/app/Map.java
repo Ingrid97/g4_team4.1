b020495fe403ed4a0414ec657a8a95db640cc72c
Brede Aksdal Sørland <Brede.Sorland@student.uib.no>
2019-03-04 14:13:25 +0100

    Endret på Map struktur, nå er det liste med liste i

--- src/main/java/inf112/skeleton/app/Map.java
+++ src/main/java/inf112/skeleton/app/Map.java
@@ -1,14 +1,22 @@
 package inf112.skeleton.app;//Created by ingridjohansen on 04/02/2019.
 
+import java.util.ArrayList;
+
 public class Map {
 
-    private IBoardObject[][] map;
+    private ArrayList[][] map;
 
     public Map(int x, int y){
         if (x < 0 || y < 0) {
                 throw new IllegalArgumentException("the x or y value of the map cannot be less than zero");
         }
-        map = new IBoardObject[x][y];
+        map = new ArrayList[x][y];
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                map[i][j] = new ArrayList<IBoardObject>();
+            }
+
+        }
     }
 
     int getX(){
@@ -25,17 +33,17 @@ public class Map {
                 throw new IllegalArgumentException("Cannot place object with these values: x= " + x +" y= "+y);
             }
         }
-        map[x][y] = c;
+        map[x][y].add(c);
 
     }
 
     //test
     IBoardObject getBoardObject(int x, int y){
-        return map[x][y];
+        return (IBoardObject) map[x][y].get(0);
     }
 
-    public IBoardObject[][] getMap() {
-        IBoardObject[][] copyOfMap = new IBoardObject[getX()][getY()];
+    public ArrayList[][] getMap() {
+        ArrayList[][] copyOfMap = new ArrayList[getX()][getY()];
         for (int x = 0; x < getX(); x++) {
             for (int y = 0; y < getY(); y++) {
                 copyOfMap[x][y] = map[x][y];
@@ -45,6 +53,6 @@ public class Map {
     }
 
     public boolean isEmpty(int x, int y) {
-        return map[x][y] == null;
+        return map[x][y].contains(null);
     }
 }
