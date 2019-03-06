57b2759c9f2a0cf81cc9440e036bb30e4f3c239e
SandraSolberg <sandsol97@gmail.com>
2019-03-06 13:51:13 +0100

    made a before method for playertest class. Sets up a map

--- src/test/java/inf112/skeleton/app/PlayerTest.java
+++ src/test/java/inf112/skeleton/app/PlayerTest.java
@@ -1,34 +1,26 @@
 package inf112.skeleton.app;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import org.junit.Before;
-import org.junit.Test;
-import org.lwjgl.input.Keyboard;
-
-import java.awt.event.KeyEvent;
-
-import static org.junit.Assert.*;
 
 /* Test for player */
 
-/*
+
 public class PlayerTest {
     private MapGUI map;
     private static Wall wallsOnTheMap;
 
 
 
-    //Set up game and objects for test in this class
+    //Method used to set up a map that can be used in all test
     @Before
 
     //can be smart to creat map here so that i don't need it to do so in every test
-    public static void setupMapBefore() throws Exception{
+    public static Map setupMapBefore() {
         Map map = new Map(10, 20);
+        return map;
     }
 
-
-
-
+/*
 
 
     @Test
