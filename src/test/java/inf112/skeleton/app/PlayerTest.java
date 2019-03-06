d5c14f3f02f712518e605e209e7986aecd975c39
SandraSolberg <sandsol97@gmail.com>
2019-03-06 15:09:36 +0100

    Setting up test for movment, but there is some issues with adding player to board

--- src/test/java/inf112/skeleton/app/PlayerTest.java
+++ src/test/java/inf112/skeleton/app/PlayerTest.java
@@ -1,107 +1,136 @@
 package inf112.skeleton.app;
 
 import org.junit.Before;
+import org.junit.Test;
+
+import java.awt.event.KeyEvent;
+
+import static inf112.skeleton.app.MapGUI.map;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /* Test for player */
 
 
 public class PlayerTest {
-    private MapGUI map;
     private static Wall wallsOnTheMap;
-
 
 
     //Method used to set up a map that can be used in all test
     @Before
-
     //can be smart to creat map here so that i don't need it to do so in every test
-    public static Map setupMapBefore() {
-        Map map = new Map(10, 20);
+    public static Map setupMapBefore(Map map) {
+        map = new Map(10, 20);
         return map;
     }
-
-/*
-
-
-    @Test
-
-    //TODO check if flags been visited
-    public void playerCollectsFlags(){
-        Player robot = new Player(10, 1);
-
-
-       //one flag
-
-      //  assertEquals(2, flagsWhichHasBeenVisited.getFlagsVisited());
-
-    }
-
-
-
-   //TODO check if an KeyBoardEvent is a legal input
-   @Test (expected = IllegalArgumentException.class)
-
-   public void illegalKeyBoardCode(){
-
-   }
-
 
 
     @Test
 
     //TODO check if legal inputs are okay
 
-    public void playerMovement(){
+    public void playerMovementUP() {
         //creating map
-        Map map = new Map(10, 20);
+        map = setupMapBefore(map);
 
         //Creating robot and  a player
-        Robot robot = new Robot(5,5, Directions.UP);
-        Player testRobot = new Player(5, robot );
-        testRobot
+        Robot robot = new Robot(10, 20, Directions.UP);
+        Player testRobot = new Player(0, robot);
 
-        assertTrue();
+        for(int x = 0; x > 10; x++ ){
+            for(int y = 0; y > 20; y++ ){
+            }
+        }
 
-
-    }
-
-
-
-
-    @Test
-
-   //Todo check if player goes through walls
-
-    public void robotDoesNotGoThroughWallsDown(){
-       Player testRobot = new Player(0, 5, 5, Directions.DOWN);
-      (testRobot.noticeWalls(Directions.DOWN))
-
-      }
-
-       if(IBoardObject  = instanceOf Wall){
-       assertFalse(testRobot.canGO = true);
-       assertTrue(testRobot.canGO = false);
+        map.add(Player(null , testRobot));
+        assertFalse(map.getBoardObject(new Position(10, 20)) instanceof Wall);
 
 
     }
 
     @Test
 
-    //TODO make the method and then make the test
+    public void playerMovmentDown() {
+        map = setupMapBefore(map);
 
-    public void givingCardsCorrectly(){
+        Robot robot = new Robot(10,20, Directions.DOWN );
+        Player testRobot = new Player(0, robot);
+        for(int x = 0; x > 10; x++ ){
+            for(int y = 0; y > 20; y++ ){
+
+            }
+        }
+        map.add(Player(null, testRobot));
+        assertFalse(map.getBoardObject(new Position(10, 20)) instanceof Wall);
+
 
     }
 
     @Test
 
-    //TODO make the method and then make the test
+    public void playerMovmentRight() {
+        map = setupMapBefore(map);
 
-    public void  testMemoryCapacityForThisPlayer(){
+        Robot robot = new Robot(10, 20, Directions.RIGHT);
+        Player testRobot = new Player(0, robot);
+        for(int i = 0; i > 10; i++ ){
+            for(int j = 0; j > 20; j++ ){
 
+            }
+        }
+        map.add(Player(null, testRobot));
+        assertFalse(map.getBoardObject(new Position(10,20)) instanceof  Wall);
+    }
+
+
+    @Test
+
+    public void playerMovmentLeft() {
+     map = setupMapBefore(map);
+
+     Robot robot = new Robot(10,20, Directions.LEFT);
+        Player testRobot = new Player(0, robot);
+        for(int i = 0; i > 10; i++ ){
+            for(int j = 0; j > 20; j++ ){
+
+            }
+        }
+        map.add(Player(null, testRobot));
+        assertFalse(map.getBoardObject(new Position(10,20)) instanceof Wall);
     }
 
 
 
 
-} */
+
+
+
+
+
+    //TODO check if an KeyBoardEvent is a legal input
+    @Test (expected = IllegalArgumentException.class)
+
+    public void illegalKeyBoardCode(KeyEvent keyEvent) {
+
+
+
+        @Test
+
+        //TODO make the method and then make the test
+
+        public void givingCardsCorrectly () {
+
+        }
+
+        @Test
+
+        //TODO make the method and then make the test
+
+        public void testMemoryCapacityForThisPlayer () {
+
+        }
+
+
+    }
+
+}
