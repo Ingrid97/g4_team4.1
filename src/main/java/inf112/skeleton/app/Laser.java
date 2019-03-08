039091e2a137622f677666394b8f388beaf4d11a
Tebs <magnussteinsvag@gmail.com>
2019-03-08 12:31:10 +0100

    Small changes to Laser

--- src/main/java/inf112/skeleton/app/Laser.java
+++ src/main/java/inf112/skeleton/app/Laser.java
@@ -1,9 +1,9 @@
 package inf112.skeleton.app;//Created by ingridjohansen on 06/02/2019.
 
 public class Laser implements IBoardObject{
-
+private Position position;
     public Laser(int x, int y){
-
+    this.position = new Position(x, y);
     }
 
 
@@ -15,6 +15,10 @@ public class Laser implements IBoardObject{
     @Override
     public int getY() {
         return 0;
+    }
+
+    public Position getPosition() {
+        return this.position;
     }
 
     @Override
