0e190e8e7e7e76001f9e9460689f2aa445192701
Tebs <magnussteinsvag@gmail.com>
2019-03-08 12:37:24 +0100

    Small changes to Laser and Void

--- src/main/java/inf112/skeleton/app/Void.java
+++ src/main/java/inf112/skeleton/app/Void.java
@@ -1,17 +1,24 @@
 package inf112.skeleton.app;//Created by ingridjohansen on 06/02/2019.
 
 public class Void implements IBoardObject {
-    public Void(int x, int y){
 
+    private Position position;
+
+    public Void(int x, int y){
+    this.position = new Position(x, y);
     }
     @Override
     public int getX() {
-        return 0;
+        return this.position.getX();
     }
 
     @Override
     public int getY() {
-        return 0;
+        return this.position.getY();
+    }
+
+    public Position getPosition() {
+        return this.position;
     }
 
     @Override
