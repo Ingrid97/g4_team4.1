18699704f539f33fe461e17421c57f959dcf0108
Henry Faltin Våge <hva033@uib.no>
2019-03-27 16:17:29 +0100

    Made a new class called MovementCardDeck for all the methods for dealing cards and such(moved out of game into the new class)

--- src/main/java/inf112/skeleton/app/Game.java
+++ src/main/java/inf112/skeleton/app/Game.java
@@ -4,30 +4,18 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 import java.util.ArrayList;
-import java.util.Collections;
 
 public class Game {
     private static ArrayList<Player> players;
-    private static ArrayList<MovementCard> theFullDeckOfAllMovementCards;
     private static boolean gameOver;
-    public static int[][] robotPositions;
-    public static int numberOfRobots;
-    private static int numberOfPlayers;
     private static Map map;
 
 
     public static void playGame() {
         // Lager alle kortene
-        setUpTheFullDeckOfCards();
-
-        players = new ArrayList<>();
-
         //leser inn map fra fil
-        robotPositions = new int[4][2];
-        numberOfRobots = 0;
-        numberOfPlayers = 0;
         players = new ArrayList<>();
-        Map map = new Map(10, 10);
+        map = new Map(10, 10);
         map = Map.makeMap("testMap1.txt", players);
         if (map == null)
             System.exit(0);
@@ -40,8 +28,8 @@ public class Game {
         new LwjglApplication(mapGUI, cfg);//instantiating MapGUI and updating the map it prints
 
         gameOver = true;
-        setUpTheFullDeckOfCards();
-        dealOutMovementCards();
+        MovementCardDeck.setUpTheFullDeckOfCards();
+        MovementCardDeck.dealOutMovementCards(players);
         while (gameOver) {
             //getting movement cards from player/s
             ArrayList<ArrayList> listOfPrioritizedListsOfMovementCardsFromPlayers = new ArrayList<>();
@@ -51,7 +39,7 @@ public class Game {
                 movementCardsToBeExecuted = players.get(i).theMovementCardsThePlayerChose();
                 listOfPrioritizedListsOfMovementCardsFromPlayers.add(movementCardsToBeExecuted);
             }
-            dealOutMovementCards();
+            MovementCardDeck.dealOutMovementCards();
 
             //playing movement cards from players
             for (int j = 0; j < 3; j++) {
@@ -218,54 +206,4 @@ public class Game {
         }
     }
 
-    private static void setUpTheFullDeckOfCards() {
-        theFullDeckOfAllMovementCards = new ArrayList<>();
-
-        // Rotate cards
-        // Left rotation, right rotation, forward movement 1
-        int priorityForLeft = 80;
-        int priorityForRight = 70;
-        int priorityForMovementForward = 490;
-        for (int i = 0; i < 18; i++) {
-            theFullDeckOfAllMovementCards.add(new MovementCard(Directions.LEFT, 0, priorityForLeft));
-            theFullDeckOfAllMovementCards.add(new MovementCard(Directions.RIGHT, 0, priorityForRight));
-            theFullDeckOfAllMovementCards.add(new MovementCard(Directions.NODIRECTION, 1, priorityForMovementForward));
-            priorityForLeft += 20;
-            priorityForRight += 20;
-            priorityForMovementForward += 20;
-        }
-
-        // Movement forward 2
-        priorityForMovementForward = 670;
-        for (int i = 0; i < 12; i++) {
-            theFullDeckOfAllMovementCards.add(new MovementCard(Directions.NODIRECTION, 2, priorityForMovementForward));
-            priorityForMovementForward += 20;
-        }
-
-        // Movement forward 3, movement backwards 1 & 180 turn
-        priorityForMovementForward = 790;
-        int priorityForMovementBackwards = 430;
-        int priorityFor180Turn = 10;
-        for (int i = 0; i < 6; i++) {
-            theFullDeckOfAllMovementCards.add(new MovementCard(Directions.NODIRECTION, 3, priorityForMovementForward));
-            theFullDeckOfAllMovementCards.add(new MovementCard(Directions.DOWN, 0, priorityFor180Turn));
-            theFullDeckOfAllMovementCards.add(new MovementCard(Directions.DOWN, 1, priorityForMovementBackwards));
-            priorityForMovementForward += 10;
-            priorityFor180Turn += 10;
-            priorityForMovementBackwards += 10;
-        }
-    }
-
-    private static void dealOutMovementCards() {
-        ArrayList<MovementCard> copy = new ArrayList<>(theFullDeckOfAllMovementCards);
-        Collections.shuffle(copy);
-
-        for (int i = 0; i < players.size(); i++) {
-
-            for (int j = 0; j < players.get(i).memoryCapacityForThisPlayer(); j++) {
-                players.get(i).giveMovementCardsToThePlayer(copy.get(j));
-                copy.remove(j);
-            }
-        }
-    }
 }
