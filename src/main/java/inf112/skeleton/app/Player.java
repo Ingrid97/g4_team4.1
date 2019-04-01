e344023b0140400a13b33798eebd60fd99061da1
Tebs <magnussteinsvag@gmail.com>
2019-04-01 15:10:08 +0200

    fix stuff that got broken with merge

--- src/main/java/inf112/skeleton/app/Player.java
+++ src/main/java/inf112/skeleton/app/Player.java
@@ -1,77 +1,128 @@
 package inf112.skeleton.app;
 
+import boardObjects.Wall;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Scanner;
 
 
 public class Player implements KeyListener {
-    private ArrayList<MovementCard> theProgramForTheRobotToExecute;
     private boolean[] flagsWhichHasBeenVisited;
     private Robot robot;
     private ArrayList<MovementCard> theCardsToChooseYourProgramFrom;
-
-
+    private ArrayList<MovementCard> programForRobotToExecute = new ArrayList<>();
 
 
 
     public Player(int numberOfFlags, Robot robot) {
         this.flagsWhichHasBeenVisited = new boolean[numberOfFlags];
-        this.theProgramForTheRobotToExecute = new ArrayList<>();
         this.robot = robot;
         theCardsToChooseYourProgramFrom = new ArrayList<>();
     }
 
+    /**
+     * Gives a movement card to the player
+     *
+     * @param card movement card to be delt to the player
+     */
     public void giveMovementCardsToThePlayer(MovementCard card) {
         this.theCardsToChooseYourProgramFrom.add(card);
     }
 
+    /**
+     * gives movement cards for the player to choose from and returns a prioritized list to execute, by taking input from player
+     *
+     * @return prioritized list of movement cars to be executed
+     */
     public ArrayList<MovementCard> theMovementCardsThePlayerChose() {
-        for (int i = 0; i < theCardsToChooseYourProgramFrom.size(); i++) {
-            System.out.println();
-            System.out.println("Alt" + (i + 1));
-            System.out.println(theCardsToChooseYourProgramFrom.get(i).toString());
-        }
-        ArrayList<MovementCard> programForRobotToExecute = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
-        boolean notDone = false;
-        System.out.println("choose your movement cards by typing the number of the card you would like to use first, then press enter ");
-        System.out.println("When you are finished write a number greater then 10");
-        while (!notDone) {
+
+        // Informasjon til bruker
+        System.out.println("Choose your movement cards by typing the number of the card you would like to use first, then press enter");
+
+        // Printer ut alle kortene først
+        for (int i = 0; i < this.robot.getMemoryCapacity(); i++) {
+            System.out.println("\nCard number " + (i + 1) + "\n" + theCardsToChooseYourProgramFrom.get(i).toString());
+        }
+
+
+        int counter = 0;
+
+        // Gjør at man bare velger antallet kort man kan ut i fra skaden roboten har tatt
+        int numberOfSlotsInRegister;
+        if (this.robot.getMemoryCapacity() > 4) {
+            numberOfSlotsInRegister = 5;
+            this.programForRobotToExecute.clear();
+        } else {
+            // Nå må MemoryCapacity være 4 eller mindre
+            numberOfSlotsInRegister = this.robot.getMemoryCapacity();
+            // Skal slette alt utenom det som er over MemoryCapacity
+            for (int i = 0; i < this.robot.getMemoryCapacity(); i++) {
+                this.programForRobotToExecute.remove(i);
+            }
+
+        }
+        HashSet<Integer> list = new HashSet<>(); // Et HashSet for å sjekke at det ikke velges samme kort flere ganger
+
+
+        do {
+            // Input from user, and add it to the list of chosen cards
+
             if (sc.hasNextInt()) {
                 int number = sc.nextInt();
-                if (number > 10) {
-                    notDone = true;
+
+                if (number <= 0 || number > this.robot.getMemoryCapacity()) {
+                    System.out.println("Illegal input: " + number);
+                    continue;
+                } else if (list.contains(number)) {
+                    System.out.println("You have already chosen this card!");
+                    continue;
                 } else {
                     programForRobotToExecute.add(theCardsToChooseYourProgramFrom.get(number - 1));
+                    counter++;
+                    list.add(number);
                 }
+            } else {
+                sc.next();
+                continue;
             }
 
-            for (int i = 0; i < programForRobotToExecute.size(); i++) {
-                System.out.println("your choices so far:");
-                System.out.println(programForRobotToExecute.get(i).toString());
-
+            // Printing out the choices the player has done this far
+            System.out.println("Your choices so far:");
+            for (MovementCard movementCard : programForRobotToExecute) {
+                System.out.println(movementCard.toString());
             }
-            System.out.println();
-        }
+            System.out.print("\nYou have " + (numberOfSlotsInRegister - counter) + " cards left to choose\n");
+        } while (counter < numberOfSlotsInRegister);
+        theCardsToChooseYourProgramFrom.clear();
 
         return programForRobotToExecute;
     }
+
 
     public int memoryCapacityForThisPlayer() {
         return this.robot.getMemoryCapacity();
     }
 
 
+    public boolean[] getFlagsWhichHasBeenVisited() {
+        return flagsWhichHasBeenVisited;
+    }
 
+    public void setFlagsWhichHasBeenVisitedTrue(int pos) {
 
+        this.flagsWhichHasBeenVisited[pos] = true;
+
+    }
 
     @Override
     public void keyTyped(KeyEvent e) {
 
     }
+
     public Directions canGO;
 
     public void keyPressed(KeyEvent key) {
